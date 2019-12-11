89cb98776575c603506c9019ab37834359323c7c
xbanki <personal@xbanki.me>
2019-12-11 09:12:53 +0200

    DevOps integration setup & test.

---
+++ src/vue/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="component-logo">
+        <p class="font-sans-heading font-semibold text-2xl leading-none text-gray-800">xBanki.me</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ComponentLogo',
+        ref: 'ComponentLogo'
+    }
+</script>
+
+// The logo component.
+//   We have three animations that can fire in the component.
+//   InitialAnimate, AnimateEnter & AnimateExit.
--- tasks/universal.webpack.js
+++ tasks/universal.webpack.js
@@ -31,7 +31,13 @@ const UniversalConfiguration = {
 			use: {
 				loader: 'babel-loader',
 				options: {
-					presets: ['@babel/preset-env'],
+					presets: [
+						['@babel/preset-env', {
+							useBuiltIns: 'usage',
+							modules: false,
+							corejs: 3
+						}]
+					],
 					plugins: ['@babel/plugin-syntax-dynamic-import']
 				}
 			}
