12843aa87146663f45ed26591a975675a4eaebb9
xbanki <contact@xbanki.me>
2021-01-19 14:26:20 +0200

    FEAT: Began work on navbar component.

---
+++ src/components/navbar/component.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="navbar">
+        <div class="container">
+            <div class="wrapper row container align-items-center"/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {}
+</script>
+
+<style lang="scss" scoped>
+    @import '../../styles/components/navbar';
+</style>
---
+++ src/components/navbar/index.ts
@@ -0,0 +1,3 @@
+import component from '@/components/navbar/component.vue';
+
+export default component;
