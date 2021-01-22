f049c141c4cb0739aa9c61b2488f24c4ebf69050
xbanki <contact@xbanki.me>
2021-01-22 16:47:07 +0200

    Wrote Menu button animations FEAT: Added basic logic to fire animation based on state. FEAT: Removed hardcoded 140ms in favor of timing the steps individually. FEAT: Described each Anime.js animation step in mounted method.

--- src/components/navbar/button-menu/button-menu.ts
+++ src/components/navbar/button-menu/button-menu.ts
@@ -6,8 +6,8 @@ import anime from 'animejs';
 export default defineComponent({
 
     setup() {
-        const button_animation = anime.timeline({ autoplay: false, duration: 120 });
-        return { button_animation }
+        const button_animation = anime.timeline({ autoplay: false });
+        return { button_animation, animation_played: false }
     },
 
     methods: {
@@ -19,12 +19,83 @@ export default defineComponent({
          * @class   ButtonMenu
          */
         activate_button(): void {
-            if (this.button_animation.began != true) this.button_animation.play();
+
+            if (this.button_animation.completed || this.animation_played === true) {
+                this.button_animation.reverse();
+                this.animation_played = true;
+            }
+
+            this.button_animation.play();
         }
     },
     mounted() {
+
         /**
-         * This is where add all the animations in the future.
+         * TOP BAR: Step 1.
+         * Slides downwards to the middle bar position.
          */
+        this.button_animation.add({
+            targets: '.button-menu-line.line-first',
+            easing: 'linear',
+            duration: 80,
+            height: 2,
+            top: 22
+        }, 0);
+
+        /**
+         * MIDDLE BAR: Step 1.
+         * Set the height to 2 pixels to fix visibility issues.
+         */
+        this.button_animation.add({
+            targets: '.button-menu-line.line-second',
+            easing: 'linear',
+            duration: 80,
+            height: 2
+        }, 0);
+
+        /**
+         * BOTTOM BAR: Step 1.
+         * Slides upwards to the middle bar position.
+         */
+        this.button_animation.add({
+            targets: '.button-menu-line.line-third',
+            easing: 'linear',
+            duration: 80,
+            height: 2,
+            top: 22
+        }, 0);
+
+        /**
+         * TOP BAR: Step 2.
+         * Rotate 42.5 degrees to the right.
+         */
+        this.button_animation.add({
+            targets: '.button-menu-line.line-first',
+            easing: 'linear',
+            duration: 60,
+            rotate: 42.5
+        }, 140);
+
+        /**
+         * MIDDLE BAR: Step 2.
+         * Hide the bar.
+         */
+        this.button_animation.add({
+            targets: '.button-menu-line.line-second',
+            easing: 'linear',
+            duration: 1,
+            opacity: 0
+        }, 140)
+
+        /**
+         * BOTTOM BAR: Step 2.
+         * Rotate 42.5 degrees to the left.
+         */
+        this.button_animation.add({
+            targets: '.button-menu-line.line-third',
+            easing: 'linear',
+            duration: 60,
+            rotate: -42.5
+        }, 140);
     }
 });
