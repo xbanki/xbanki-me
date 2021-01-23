4fcb268a030698cfd2b3639074d340cddad58bff
xbanki <contact@xbanki.me>
2021-01-23 23:34:10 +0200

    Added state management to Menu Button component. FEAT: Removed setup method in favor of defining both animation_state and button_animation in the data property/ method. FEAT: Described animation state enum and interface. FEAT: Added Anime.js change methods to handle changing the state. FEAT: Documented all of the above mentioned points.

--- src/components/navbar/button-menu/button-menu.ts
+++ src/components/navbar/button-menu/button-menu.ts
@@ -3,11 +3,97 @@
 import { defineComponent } from 'vue';
 import anime from 'animejs';
 
-export default defineComponent({
+/**
+ * All possible animation states which are used to emit certain events
+ */
+enum AnimationStates {
 
-    setup() {
+    /**
+     * Self explanatory. This is the active state when the button has not been activated.
+     * This state is not returned to or set if the button has been interacted with.
+     * @class ButtonMenu
+     */
+    STATE_INITIAL,
+
+    /**
+     * True if animation has been activated once, but has been reverted to default state.
+     * @class ButtonMenu
+     */
+    STATE_IDLE_INITIAL,
+
+    /**
+     * True if animation has been activated and completed.
+     * @class ButtonMenu
+     */
+    STATE_IDLE_ACTIVATED,
+
+    /**
+     * True if animation is currently animating. Both normal and reverse modes.
+     * @class ButtonMenu
+     */
+    STATE_ACTIVE
+}
+
+/**
+ * Menu Button animation state object. Is updated if the user interacts in any way.
+ * @class ButtonMenu
+ */
+interface ButtonState {
+
+    /**
+     * Denotes if the animation has played at least once.
+     * @class ButtonMenu
+     */
+    animation_played: boolean;
+
+    /**
+     * Current animation state which get's watched and emitted
+     * only if certain states are active.
+     * @class ButtonMenu
+     */
+    current_state: AnimationStates;
+
+    /**
+     * Total number of times the animation has finished. One loop
+     * equates to two finishes.
+     * @class ButtonMenu
+     */
+    total_animation_loops: number;
+
+    /**
+     * Is `true` if the button is in the `STATE_IDLE_ACTIVATED` state.
+     * @class ButtonMenu
+     */
+    currently_active: boolean;
+}
+
+export default defineComponent({
+    data() {
+
+        /**
+         * Animation state that controls the functionality of the
+         * button, along with emitting an event depending on the
+         * event that is currently active.
+         * @class ButtonMenu
+         */
+        const animation_state: ButtonState = {
+            current_state: AnimationStates.STATE_INITIAL,
+            animation_played: false,
+            currently_active: false,
+            total_animation_loops: 0
+        }
+
+        /**
+         * `animejs` based animation object that will get populated
+         * in the component `mounted` method.
+         * @class ButtonMenu
+         */
         const button_animation = anime.timeline({ autoplay: false });
-        return { button_animation, animation_played: false }
+
+        return {
+            button_animation,
+            animation_state
+        }
     },
 
     methods: {
@@ -20,11 +106,12 @@ export default defineComponent({
          */
         activate_button(): void {
 
-            if (this.button_animation.completed || this.animation_played === true) {
-                this.button_animation.reverse();
-                this.animation_played = true;
-            }
-
+            /**
+             * TEMP (xbanki):
+             *  Currently does not take in to account the state.
+             *  This will be replaced later down the line.
+             */
+            if (this.animation_state.animation_played) this.button_animation.reverse();
             this.button_animation.play();
         }
     },
@@ -94,5 +181,37 @@ export default defineComponent({
             duration: 60,
             rotate: -42.5
         }, 125);
+
+        /**
+         * STATE MANAGEMENT
+         *
+         * We set the state to `STATE_ACTIVE` when the button is activated and the animation is fired.
+         */
+        this.button_animation.changeBegin = () => this.animation_state.current_state = AnimationStates.STATE_ACTIVE;
+
+        this.button_animation.changeComplete = () => {
+
+            /**
+             * Based on the completed number of loops the animation has completed (normal & reverse),
+             * we determine wether `animation_state.currently_active` should be true or not.
+             * If this function returns 0, it is even. Otherwise, it is an odd number.
+             */
+            if (this.animation_state.total_animation_loops % 2 == 0) this.animation_state.currently_active = true;
+            else this.animation_state.currently_active = false;
+
+            /**
+             * If this animation has finished, we set the played flag to true.
+             * We also add to the number of loops the animation has completed.
+             */
+            if (!this.animation_state.animation_played) this.animation_state.animation_played = true;
+            this.animation_state.total_animation_loops++;
+
+            /**
+             * We set the current state based on if the component is active in the state object.
+             */
+            this.animation_state.currently_active
+                ? this.animation_state.current_state = AnimationStates.STATE_IDLE_ACTIVATED
+                : this.animation_state.current_state = AnimationStates.STATE_IDLE_INITIAL;
+        }
     }
 });
