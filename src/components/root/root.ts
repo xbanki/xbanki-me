745e099d4e10bc85296809468b921ff4b3fd091c
xbanki <contact@xbanki.me>
2021-01-28 01:38:52 +0200

    Added missing root related components. FEAT: Added temporary content component which will be used as a placeholder for the forseeable future. FEAT: Added Root component which has been split in to separate files just like every other component.

---
+++ src/components/root/root.ts
@@ -0,0 +1,10 @@
+import contentComponent from '@/components/root/content/content.vue';
+// import navbarComponent from '@/components/navbar/navbar.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components: {
+        contentComponent
+        // navbarComponent
+    }
+});
---
+++ src/components/root/root.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <!-- Navigation bar component //-->
+    <!-- <navbar-component/> //-->
+
+    <!-- Main wrapper element //-->
+    <main class="root container">
+
+        <!-- Temporary content component //-->
+        <content-component/>
+    </main>
+</template>
+
+<script lang="ts" src="./root.ts"></script>
+<style lang="scss" src="./root.scss"></style>
