227bde6522c651bc02342a1d22de8397b18bcece
xbanki <contact@xbanki.me>
2021-01-30 04:22:12 +0200

    Added active styles to Menu Button component. CHORE: Also added a 12px padding to the right side to match Bootstrap 5 Grid.

--- src/components/navbar/button-menu/button-menu.vue
+++ src/components/navbar/button-menu/button-menu.vue
@@ -1,7 +1,7 @@
 <template>
 
     <!-- Button "bounding box" or wrapper/ container //-->
-    <span class="component-button-menu" v-on:click="activate_button">
+    <span class="component-button-menu" v-bind:class="{ active: animation_state.currently_active }" v-on:click="activate_button">
 
         <!-- The burger menu lines //-->
         <span class="button-menu-line line-first"/>
