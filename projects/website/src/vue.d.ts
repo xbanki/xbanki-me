5e127ee6dc69e70259e31fcfede666a6c9dacb15
xbanki <contact@xbanki.me>
2025-03-19 14:55:43 +0200

    chore(website): Add TypeScript config & Vue type decls

---
+++ projects/website/src/vue.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Vue & Vite global type declarations.
+ *
+ *    @copyright Copyright (c) 2025, xbanki <contact@xbanki.me>
+ *               Licensed under MIT License.
+ *               See LICENSE for more details.
+ *    @author    xbanki <contact@xbanki.me>
+ *    @since     1.0.0
+ *    @version   1.0.0
+ */
+
+/* eslint-disable */
+
+/// <reference types="vite/client" />
+declare module '*.vue' {
+    import { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+}
