c43ac09ec31ddcc7bb87c6c79333ff161a445b8c
xbanki <contact@xbanki.me>
2025-04-27 14:09:46 +0300

    chore(stealthmode): Add Storybook theme deps & dark mode support

--- projects/stealthmode/.storybook/preview.ts
+++ projects/stealthmode/.storybook/preview.ts
@@ -9,8 +9,14 @@
  */
 
 import type { Preview } from '@storybook/vue3';
+import { themes } from '@storybook/theming';
 
 const preview: Preview = {
+    parameters: {
+        docs: {
+            theme: themes.dark
+        }
+    },
     tags: ['autodocs']
 };
 
