56e60aabb4112e21effa52f9809311028a8b80fa
xbanki <contact@xbanki.me>
2025-07-13 20:39:28 +0300

    feat(ui): Add Storybook Stories

--- projects/ui/stories/MatrixReveal.stories.ts
+++ projects/ui/stories/MatrixReveal.stories.ts
@@ -11,12 +11,20 @@
 
 import type { StoryObj, Meta } from '@storybook/vue3-vite';
 
-import { MatrixReveal } from '@/library.ts';
+import { MatrixRevealState, MatrixReveal } from '@/library.ts';
 
 type Story = StoryObj<typeof MatrixReveal>;
 
 /**
  * [Source Code](https://github.com/xbanki/xbanki-me/blob/main/projects/ui/src/matrix-reveal/component.vue)
+ *
+ * # Overview
+
+ * The `MatrixReveal` component is an animated text effect designed to add visual
+ * flair to text elements such as logos, articles, or other UI components. It
+ * accepts any DOM structure in the `default` slot, as long as the content
+ * resolves to animatable text nodes. Overall, `MatrixReveal` is designed for
+ * maximum flexibility.
  */
 const Meta: Meta<typeof MatrixReveal> = {
     title: 'Components/MatrixReveal',
@@ -31,7 +39,7 @@ const Meta: Meta<typeof MatrixReveal> = {
 };
 
 /**
- * `@TODO(xbanki): Document.`
+ * Orphan text nodes are supported directly.
  */
 export const Default: Story = {
     parameters: {
@@ -44,30 +52,7 @@ export const Default: Story = {
 };
 
 /**
- * `@TODO(xbanki): Document.`
- */
-export const NestedDOMTree: Story = {
-    parameters: {
-        slots: {
-            default: {
-                template: `
-                    <span>
-                        Nested
-                        <span>
-                            DOM
-                            <span>
-                                Tree
-                            </span>
-                        </span>
-                    </span>
-                `
-            }
-        }
-    }
-};
-
-/**
- * `@TODO(xbanki): Document.`
+ * Direct animation between orphaned text nodes is supported.
  */
 export const ContentReactive: Story = {
     render: args => ({
@@ -82,11 +67,111 @@ export const ContentReactive: Story = {
             <MatrixReveal v-bind="args">
                 {{ !toggle ? alpha : beta }}
             </MatrixReveal>
-            <button v-on:click="toggle = !toggle">
+            <button class="mt-4" v-on:click="toggle = !toggle">
                 Toggle Label
             </button>
         `
     })
 };
 
+/**
+ * Any DOM node tree is supported; nodes with text content or child text nodes
+ * are animated.
+ */
+export const NestedDOMTree: Story = {
+    parameters: {
+        slots: {
+            default: {
+                template: `
+                    <div>
+                        Neste
+                        <div>
+                            DOM
+                            <div>
+                                Tree
+                            </div>
+                        </div>
+                    </div>
+                `
+            }
+        }
+    }
+};
+
+/**
+ * Reactive DOM structures are supported and can dynamically take any form.
+ */
+export const ReactiveDOMTree: Story = {
+    render: args => ({
+        components: { MatrixReveal },
+        data() {
+            const toggle = true;
+            return { toggle, args };
+        },
+        template: `
+            <MatrixReveal v-bind="args">
+                <span v-if="toggle">
+                    Click
+                    <span>
+                        the button.
+                    </span>
+                </span>
+                <span v-else>
+                    Hey, the content
+                    <p>
+                        changed!
+                    </p>
+                </span>
+            </MatrixReveal>
+            <button class="mt-4" v-on:click="toggle = !toggle">
+                Toggle DOM Tree
+            </button>
+        `
+    })
+};
+
+/**
+ * The component emits events reflecting the animation timeline state.
+ */
+export const AnimationStateEvents: Story = {
+    render: args => ({
+        components: { MatrixReveal },
+        data() {
+            const state = MatrixRevealState.INITIAL;
+            const toggle = true;
+            const index = 0;
+            return { toggle, state, index, args };
+        },
+        computed: {
+            convertEventToString() {
+                switch (this.state) {
+                    case MatrixRevealState.INITIAL:
+                        return 'INITIAL';
+                    case MatrixRevealState.IDLE:
+                        return 'IDLE';
+                    case MatrixRevealState.OUT:
+                        return 'OUT';
+                    case MatrixRevealState.IN:
+                        return 'IN';
+                    default:
+                        return 'NONE';
+                }
+            }
+        },
+        template: `
+            <MatrixReveal v-bind="args" v-on:stateChange="state = $event">
+                Component changes: {{ index }}
+            </MatrixReveal>
+            <p class="dark:text-white">
+                Current animation state:
+                <samp>
+                    {{ convertEventToString }}
+                </samp>
+            </p>
+            <button class="mt-4" v-on:click="index += 1">
+                Force Re-Render
+            </button>
+        `
+    })
+};
 export default Meta;
