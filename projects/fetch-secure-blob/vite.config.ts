749a2a8765d8090e849f2321d96f4d7a0a879f5e
xbanki <contact@xbanki.me>
2025-08-13 17:16:22 +0300

    feat(repo): Initialize fetch-secure-blob package

---
+++ projects/fetch-secure-blob/vite.config.ts
@@ -0,0 +1,40 @@
+/**
+ * Vite configuration.
+ *
+ *    @copyright Copyright (c) 2025, xbanki <contact@xbanki.me>
+ *               Licensed under MIT License.
+ *               See LICENSE for more details.
+ *    @author    xbanki <contact@xbanki.me>
+ *    @since     1.1.0
+ *    @version   1.1.0
+ */
+
+import { resolve, join } from "node:path";
+import { defineConfig } from "vite";
+
+import pluginTypeScriptDefinitions from "vite-plugin-dts";
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: resolve(join(process.cwd(), "src", "library.ts")),
+			fileName: "fetch-secure-blob",
+			name: "fetch-secure-blob",
+		},
+		minify: "terser",
+		sourcemap: true,
+		terserOptions: {
+			mangle: true,
+		},
+	},
+	plugins: [
+		pluginTypeScriptDefinitions({
+			rollupTypes: true,
+		}),
+	],
+	resolve: {
+		alias: {
+			"@": resolve(join(process.cwd(), "src")),
+		},
+	},
+});
