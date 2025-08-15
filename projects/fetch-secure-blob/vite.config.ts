f3a8dbaf83218d8e9d4926ac66ba22efae13dbea
xbanki <contact@xbanki.me>
2025-08-15 15:05:04 +0300

    chore(fetch-secure-blob): Finalize build config, deps

--- projects/fetch-secure-blob/vite.config.ts
+++ projects/fetch-secure-blob/vite.config.ts
@@ -9,32 +9,44 @@
  *    @version   1.1.0
  */
 
-import { resolve, join } from "node:path";
-import { defineConfig } from "vite";
+import { resolve, join } from 'node:path';
+import { defineConfig } from 'vite';
 
-import pluginTypeScriptDefinitions from "vite-plugin-dts";
+import pluginTypeScriptDefinitions from 'vite-plugin-dts';
 
 export default defineConfig({
-	build: {
-		lib: {
-			entry: resolve(join(process.cwd(), "src", "library.ts")),
-			fileName: "fetch-secure-blob",
-			name: "fetch-secure-blob",
-		},
-		minify: "terser",
-		sourcemap: true,
-		terserOptions: {
-			mangle: true,
-		},
-	},
-	plugins: [
-		pluginTypeScriptDefinitions({
-			rollupTypes: true,
-		}),
-	],
-	resolve: {
-		alias: {
-			"@": resolve(join(process.cwd(), "src")),
-		},
-	},
+    build: {
+        lib: {
+            entry: resolve(join(process.cwd(), 'src', 'library.ts')),
+            fileName: 'fetch-secure-blob',
+            name: 'fetch-secure-blob',
+        },
+        minify: 'terser',
+        rollupOptions: {
+            external: [
+                'node:fs/promises',
+                'node:path',
+            ],
+        },
+        sourcemap: true,
+        terserOptions: {
+            mangle: true,
+        },
+    },
+    plugins: [
+        pluginTypeScriptDefinitions({
+            rollupTypes: true,
+        }),
+    ],
+    optimizeDeps: {
+        exclude: [
+            'node:fs/promises',
+            'node:path',
+        ],
+    },
+    resolve: {
+        alias: {
+            '@': resolve(join(process.cwd(), 'src')),
+        },
+    },
 });
