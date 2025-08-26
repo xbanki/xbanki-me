d68cb8a4ea5a5be32dedf42cd7ed5e6697d1ef4d
xbanki <contact@xbanki.me>
2025-08-26 12:31:35 +0300

    chore(repo): Merge main into ui

---
+++ projects/fetch-secure-blob/src/constants.ts
@@ -0,0 +1,16 @@
+/**
+ * Constant declarations required by this plugin.
+ *
+ *    @copyright Copyright (c) 2025, xbanki <contact@xbanki.me>
+ *               Licensed under MIT License.
+ *               See LICENSE for more details.
+ *    @author    xbanki <contact@xbanki.me>
+ *    @since     1.0.0
+ *    @version   1.2.0
+ */
+
+export const PLUGIN_NAME = 'fetch-secure-blob';
+
+export const CACHE_FILE_NAME = 'blob-state.json';
+
+export const DEFAULT_TTL = 24 * 60 * 1000;
