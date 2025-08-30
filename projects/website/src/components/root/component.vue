19e89e876b3a45b0270eb7e85a1f422bc4853f39
xbanki <contact@xbanki.me>
2025-08-30 20:25:47 +0300

    feat(website): Integrate vue router

--- projects/website/src/components/root/component.vue
+++ projects/website/src/components/root/component.vue
@@ -1,6 +1,23 @@
+<!--
+ // Root component declaration.
+ //
+ //    Copyright: Copyright (c) 2025, xbanki <contact@xbanki.me>
+ //               Licensed under MIT License.
+ //               See LICENSE for more details.
+ //    Author:    xbanki <contact@xbanki.me>
+ //    Since:     1.0.0
+ //    Version:   1.0.0
+ //-->
+
 <template>
     <!-- Root component. //-->
-    <main />
+    <main>
+      <!-- Router content wrapping element. //-->
+      <section>
+        <!-- Router slot renderer. //-->
+        <RouterView/>
+      </section>
+    </main>
 </template>
 
 <style lang="css">
