656646e1b3b64312dbd144d9d10524d2254aaac7
xbanki <contact@xbanki.me>
2025-09-04 13:30:28 +0300

    fix(ui): Build failure, git conflict artefacts

--- projects/ui/src/article/lib/constants.ts
+++ projects/ui/src/article/lib/constants.ts
@@ -13,14 +13,11 @@
  *    @version   1.0.0
  */
 
-<<<<<<<< HEAD:projects/ui/src/logo/lib/constants.ts
 import type { ILogoPropsOptional } from '@/logo/lib/types.ts';
 
 export const DEFAULT_LOGO_PROPS_OPTIONAL: ILogoPropsOptional = {
     segments: [],
 };
-========
 import type { IArticleProps } from '@/article/lib/types.ts';
 
 export const DEFAULT_ARTICLE_PROPS_OPTIONAL: IArticleProps = {};
->>>>>>>> ui:projects/ui/src/article/lib/constants.ts
