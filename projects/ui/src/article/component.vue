45ad0426e9d45e1e71f3fd2bead9dd3231111ad9
xbanki <contact@xbanki.me>
2025-09-04 13:22:55 +0300

    chore(website): Upstream branch changes

---
+++ projects/ui/src/article/component.vue
@@ -0,0 +1,25 @@
+<!--
+ // Article Component declaration.
+ //
+ //    Copyright: Copyright (c) 2025, xbanki <contact@xbanki.me>
+ //               Licensed under MIT License.
+ //               See LICENSE for more details.
+ //    Author:    xbanki <contact@xbanki.me>
+ //    Since:     1.0.0
+ //    Version:   1.0.0
+ //-->
+
+<template>
+  <!-- Article component wrapping element. //-->
+  <article class="prose" />
+</template>
+
+<script lang="ts" setup>
+import type { ArticleProps } from '@/article/lib/types.ts';
+import { DEFAULT_ARTICLE_PROPS_OPTIONAL } from '@/article/lib/constants.ts';
+
+const props = withDefaults(
+    defineProps<ArticleProps>(),
+    DEFAULT_ARTICLE_PROPS_OPTIONAL,
+);
+</script>
