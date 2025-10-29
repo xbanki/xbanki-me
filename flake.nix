bc3e519a31933451fe2a47bacfe4eb958e99b75e
xbanki <contact@xbanki.me>
2025-10-29 06:51:44 +0000

    feat(repo): Add direnv support, Nix devshell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+#
+# NixOS development shell.
+#
+# Copyright: Copyright (c) 2025 xbanki <contact@xbanki.me>
+#            Licensed under MIT License.
+#            See LICENSE for mode details.
+# Author:    xbanki <contact@xbanki.me>
+#
+
+{
+  description = "xBanki.me development shell";
+  inputs = {
+    nixpkgs.url     = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs { inherit system; };
+
+      in {
+        devShells.default = pkgs.mkShell {
+	  shellHook = "corepack enable --install-directory ~/.local/bin";
+	  name = "xbanki-me-devshell";
+	  buildInputs = [
+            pkgs.nodejs_22
+	  ];
+	};
+      }
+   );
+}
