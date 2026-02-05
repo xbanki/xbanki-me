fa3035e6cd65b0a200ffe703b6a34e6ccd747c7e
xbanki <contact@xbanki.me>
2026-02-05 08:45:12 +0000

    chore(ui): Remove components, update dependencies & license years

--- projects/ui/src/library.ts
+++ projects/ui/src/library.ts
@@ -3,7 +3,7 @@
  *
  * Minimal grayscale-inspired UI component library.
  *
- *    @copyright Copyright (c) 2025, xbanki <contact@xbanki.me>
+ *    @copyright Copyright (c) 2025 - 2026, xbanki <contact@xbanki.me>
  *               Licensed under MIT License.
  *               See LICENSE for more details.
  *    @author    xbanki <contact@xbanki.me>
@@ -15,12 +15,8 @@
 import { EMatrixRevealAnimationState } from '@/matrix-reveal/lib/types.ts';
 import ComponentMatrixReveal from '@/matrix-reveal/component.vue';
 
-import ComponentArticle from '@/article/component.vue';
-
 import { EButtonVariant } from '@/button/lib/types.ts';
 import ComponentButton from '@/button/component.vue';
-
-import ComponentLogo from '@/logo/component.vue';
 
 import '@/css/module.css';
 
@@ -28,7 +24,5 @@ export {
     EMatrixRevealAnimationState as MatrixRevealState,
     EButtonVariant as ButtonVariant,
     ComponentMatrixReveal,
-    ComponentArticle,
     ComponentButton,
-    ComponentLogo,
 };
