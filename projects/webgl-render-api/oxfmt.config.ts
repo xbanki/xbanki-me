698c9e2e480a7e1dce07d9140a9d72c4c5d3734a
xbanki <contact@xbanki.me>
2026-04-12 02:06:23 +0300

    style(repo): Migrate from ESLint & Biome to Oxfmt & Oxlint

---
+++ projects/webgl-render-api/oxfmt.config.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026, xbanki <contact@xbanki.me>
+ * Licensed under MIT License.
+ * See LICENSE for more details.
+ */
+
+import {typescript, base, json, css} from '@xbanki-me/config/oxfmt';
+import {defineConfig} from 'oxfmt';
+import {merge} from 'ts-deepmerge';
+
+export default defineConfig(merge(base, typescript, json, css));
